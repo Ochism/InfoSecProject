dc6e31d06af2cb55ab5bb73054f1791d6f5a13dd
kurtiskusz <kurtiskusz@gmail.com>
2018-04-11 19:50:34 -0400

    finished body classifier training

---
+++ Classifier/build_body_csv.py
@@ -0,0 +1,42 @@
+import email.parser
+import csv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bodies', required=True,
+            help='Directory that contains the bodies to process')
+parser.add_argument('--label', required=True,
+            help='Label file for classification')
+parser.add_argument('--output', required=True,
+            help='Name of the output csv file')
+args = parser.parse_args()
+
+print(args.bodies)
+body_dir = args.bodies if args.bodies[-1] == '/' else args.bodies + '/'
+label = args.label
+output = args.output if args.output[-4:] == '.csv' else args.output + '.csv'
+classification_map = {'0': 'spam', '1': 'not spam'}
+
+with open(output, 'w') as classifier:
+    csv_writer = csv.writer(classifier, dialect='excel')
+
+    with open(label, 'r') as label:
+        row_nbr = 0
+        for line in label:
+            parts = line.split(' ')
+            classification = parts[0]
+            filename = parts[1].strip('\n')
+            filename = body_dir + filename
+            with open(filename, 'r') as body_file:
+                try:
+                    for chunk in body_file:
+                        try:
+                            row_nbr = row_nbr + 1
+                            chunk = chunk.replace('\n', '')
+                            csv_writer.writerow((chunk, classification_map[classification]))
+                            print('row #' + str(row_nbr) + ' written')
+                        except:
+                            pass
+                except:
+                    pass
+
---
+++ Classifier/extract_bodies.py
@@ -0,0 +1,86 @@
+import email.parser 
+import os, sys, stat
+import shutil
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def process_text(body, dstfile):
+    cleaned_text = strip_tags(body)
+    cleaned_text = cleaned_text.replace('\n','')
+    cleaned_text = cleaned_text.replace('\t','')
+    cleaned_text = cleaned_text.replace('\r','')
+    
+    for chunk in chunkstring(cleaned_text, 1000):
+        stripped = chunk.lstrip()
+        stripped = chunk.rstrip()
+        dstfile.write(stripped + '\n')
+
+    return cleaned_text
+
+def ExtractSubPayload (filename):
+    '''
+    Extract the body from the .eml file.
+    '''
+    if not os.path.exists(filename): # dest path doesnot exist
+        print("ERROR: input file does not exist:", filename)
+        os.exit(1)
+    fp = open(filename)
+    msg = email.message_from_file(fp)
+    payload = msg.get_payload()
+    if type(payload) == type(list()) :
+        payload = payload[0] # only use the first part of payload
+    if type(payload) != type('') :
+        payload = str(payload)
+    
+    return payload
+
+def ExtractBodyFromDir ( srcdir, dstdir ):
+    '''
+    Extract the body information from all .eml files in the srcdir and 
+    save the file to the dstdir with the same name.
+    '''
+    if not os.path.exists(dstdir): # dest path doesnot exist
+        os.makedirs(dstdir)  
+    files = os.listdir(srcdir)
+    for i,file in enumerate(files):
+        srcpath = os.path.join(srcdir, file)
+        dstpath = os.path.join(dstdir, file)
+        src_info = os.stat(srcpath)
+        if stat.S_ISDIR(src_info.st_mode): # for subfolders, recurse
+            ExtractBodyFromDir(srcpath, dstpath)
+        else:
+            body = ExtractSubPayload (srcpath)
+            dstfile = open(dstpath, 'w')
+            body = process_text(body, dstfile)
+            dstfile.close()
+
+print('Input source directory: ') #ask for source and dest dirs
+srcdir = raw_input()
+if not os.path.exists(srcdir):
+    print 'The source directory %s does not exist, exit...' % (srcdir)
+    sys.exit()
+# dstdir is the directory where the content .eml are stored
+print 'Input destination directory: ' #ask for source and dest dirs
+dstdir = raw_input()
+if not os.path.exists(dstdir):
+    print 'The destination directory is newly created.'
+    os.makedirs(dstdir)
+
+###################################################################
+ExtractBodyFromDir ( srcdir, dstdir )
