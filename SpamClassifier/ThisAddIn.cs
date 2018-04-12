b458c51b30454208ff1645e542b99d48be00c7a1
Ethan Knez <knez.33@buckeyemail.osu.edu>
2018-04-12 00:12:51 -0400

    Pushed changes to Outlook Add-In

--- SpamClassifier/ThisAddIn.cs
+++ SpamClassifier/ThisAddIn.cs
@@ -1,39 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
+using System;
 using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
 using System.Windows.Forms;
+using IBM.WatsonDeveloperCloud.NaturalLanguageClassifier.v1;
+using IBM.WatsonDeveloperCloud.NaturalLanguageClassifier.v1.Model;
+using System.Net;
 
 namespace SpamClassifier
 {
     public partial class ThisAddIn
     {
-        // Ensure GC does not free needed memory
-        Outlook.Inspectors inspectors;
+        // Global constants and variables
+        private string username = "a477516a-4cdf-4080-93bc-064265ec1643";
+        private string password = "4JnCcEcxFDjM";
+        private string subjectClassifierID = "2fc15ax329-nlc-819";
+        private string bodyClassifierID = "ab2c7bx342-nlc-368";
+        NaturalLanguageClassifierService _naturalLanguageClassifierService;
 
         private void ThisAddIn_Startup(object sender, EventArgs e)
         {
+            // For Windows 7 and later
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Set the credentials
+            _naturalLanguageClassifierService = new NaturalLanguageClassifierService();
+            _naturalLanguageClassifierService.SetCredential(username, password);
+
             this.Application.NewMail += new Outlook.ApplicationEvents_11_NewMailEventHandler(NewMailMethod);
-            //inspectors = this.Application.Inspectors;
-            //inspectors.NewInspector += new Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
         }
 
         private void ThisAddIn_Shutdown(object sender, EventArgs e)
         {
-            // Note: Outlook no longer raises this event. If you have code that 
-            // must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
         }
+
         void NewMailMethod()
         {
+            // Declare our inbox and junk folder
             Outlook.MAPIFolder inBox = Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
-            Outlook.MAPIFolder junkBox = Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderJunk);
-            Outlook.Items items = (Outlook.Items)inBox.Items;
+            Outlook.MAPIFolder junkFolder = Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderJunk);
+            Outlook.Items items = inBox.Items;
             Outlook.MailItem moveMail = null;
-            items.Restrict("[UnRead] = true");
-            Outlook.MAPIFolder destFolder = junkBox;
+
+            // TODO: Iterate only over unread messages
             foreach (object eMail in items)
             {
                 try
@@ -41,10 +49,19 @@ namespace SpamClassifier
                     moveMail = eMail as Outlook.MailItem;
                     if (moveMail != null)
                     {
-                        string titleSubject = (string)moveMail.Subject;
-                        moveMail.Move(destFolder);
-                        moveMail.Subject = "This  email is evil" + moveMail.Subject;
-                        moveMail.Body = "This email is evil" + moveMail.Body;
+                        // TODO: Incorporate both subject and body for our text field
+                        ClassifyInput classifyInput = new ClassifyInput
+                        {
+                            Text = moveMail.Subject
+                        };
+
+                        Classification classifyResult = _naturalLanguageClassifierService.Classify(subjectClassifierID, classifyInput);
+                        if (classifyResult.TopClass == "spam")
+                        {
+                            // TODO: Include messages in spam subject/body
+                            moveMail.Move(junkFolder);
+                            moveMail.Body = classifyResult.TopClass + moveMail.Body;
+                        }
                     }
                 }
                 catch (Exception ex)
@@ -53,23 +70,7 @@ namespace SpamClassifier
                 }
             }
         }
-
-        void Inspectors_NewInspector(Outlook.Inspector Inspector)
-        {
-            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
-            if (mailItem != null)
-            {
-                if (mailItem.EntryID != null)
-                {
-                    //api call
-
-                    //if(its evil)
-                    mailItem.Move(this.Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderJunk));
-                    mailItem.Subject = "This  email is evil" + mailItem.Subject;
-                    mailItem.Body = "This email is evil" + mailItem.Body;
-                }
-            }
-        }
+        
         #region VSTO generated code
 
         /// <summary>
